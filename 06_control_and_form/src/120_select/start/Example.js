0c4261f801c868a604c57716564f0502802d3209
tackernao0522 <cheap_trick_magic@yahoo.co.jp>
2022-08-18 15:35:22 +0900

    69. [Form] プルダウンの作成方法

--- 06_control_and_form/src/120_select/start/Example.js
+++ 06_control_and_form/src/120_select/start/Example.js
@@ -1,11 +1,27 @@
-const Example = () => {
-  return (
-    <p style={{ textAlign: "center" }}>
-      startフォルダの内容が表示されます。
-      <br />
-      練習用に使ってください！
-    </p>
-  );
-};
+import { useState } from 'react'
 
-export default Example;
+const Example = () => {
+  const [selected, setSelected] = useState('Banana')
+  const OPTIONS = ['Apple', 'Banana', 'Cherry']
+
+  return (
+    <>
+      <p style={{ textAlign: 'center' }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        {OPTIONS.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
+        ))}
+      </select>
+      <div>選択された果物: {selected}</div>
+    </>
+  )
+}
+
+export default Example
--- README11.md
+++ README11.md
@@ -342,3 +342,37 @@ const Example = () => {
 
 export default Example
 ```
+
+## 69. [Form] プルダウンの作成方法
+
++ `06_control_and_form/src/120_select/start/Example.js`を編集<br>
+
+```js:Example.js
+import { useState } from 'react'
+
+const Example = () => {
+  const [selected, setSelected] = useState('Banana')
+  const OPTIONS = ['Apple', 'Banana', 'Cherry']
+
+  return (
+    <>
+      <p style={{ textAlign: 'center' }}>
+        startフォルダの内容が表示されます。
+        <br />
+        練習用に使ってください！
+      </p>
+
+      <select value={selected} onChange={(e) => setSelected(e.target.value)}>
+        {OPTIONS.map((opt) => (
+          <option key={opt} value={opt}>
+            {opt}
+          </option>
+        ))}
+      </select>
+      <div>選択された果物: {selected}</div>
+    </>
+  )
+}
+
+export default Example
+```
